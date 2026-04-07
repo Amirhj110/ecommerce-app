a1382d5921b9fdf87352ff2467a6274e965a0f63
Amir <aamerhj110@gmail.com>
2026-04-08 01:51:14 +0500

    fix: remove SSL requirement for Railway internal PostgreSQL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,7 +77,7 @@ DATABASE_URL = config(
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
 else:
     DATABASES = {
